46896aaa7b59128932f3f6042a886c6298889820
Omar Tonsy <32075386+omartonsy02@users.noreply.github.com>
2023-08-23 00:04:56 +0300

    Add files via upload

---
+++ lib/calc.py
@@ -0,0 +1,14 @@
+def plus(a, b):
+    return a + b
+
+
+def min(a, b):
+    return a - b
+
+
+def times(a, b):
+    return a * b
+
+
+def div(a, b):
+    return a / b
---
+++ lib/main.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import calc
+
+if __name__ == '__main__':
+    a = 12
+    b = 3
+    print(calc.plus(a, b))
+    print(calc.min(a, b))
+    print(calc.times(a, b))
+    print(calc.div(a, b))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
